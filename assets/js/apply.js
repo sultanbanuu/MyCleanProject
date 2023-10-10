a35d18d38cbae6651cb4a74f511b2e12df10c745
sultanbanuu <sultanbanuahmett@gmail.com>
2023-10-10 12:45:51 +0400

    first commit

---
+++ assets/js/apply.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById("apply-modal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("apply-modal").style.display = "none";
+  }
